05af79070a42869cbb63973c7c62e3ba3ae70113
xiaojun <xiaojun@zhongan.com>
2016-01-11 17:55:17 +0800

    init

---
+++ Array2csv.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace xiaojun\help;
+
+/**
+ * 将数组转换成csv文件并存储
+ * Class Array2csv
+ * @package xiaojun\help
+ */
+class Array2csv
+{
+    public static function run(array $rows, $saveFile, array $headers = [], $delimiter=',')
+    {
+        $handle = fopen($saveFile, 'w+');
+        if(!$handle)
+        {
+            return false;
+        }
+        if (count($headers))
+        {
+            fputcsv($handle, $headers, $delimiter);
+        }
+        foreach ($rows as $row)
+        {
+            fputcsv($handle, $row, $delimiter);
+        }
+        fclose($handle);
+        return true;
+    }
+}
---
+++ Curl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace xiaojun\help;
+
+/**
+ * curl 方法的封装
+ * Class Curl
+ * @package xiaojun\help
+ */
+class Curl
+{
+    //TODO
+}
